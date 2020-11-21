da49dd9cd41a2532d6b4a43011cbfca7aa6c43cf
Joshua Riggleman <jriggleman16@gmail.com>
2020-11-20 20:09:04 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello world, this is Josh! ");
+		System.out.print('X');
+
+	}
+
+}
